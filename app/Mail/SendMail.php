76ed5b540d3c0c96dbe2744cb4eb97430ab6368e
Muhammad Rafli <raflylagitobat09@gmail.com>
2023-03-01 17:23:44 +0700

    update queue send

---
+++ app/Mail/SendMail.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SendMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $data;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+
+    /**
+     * Get the message envelope.
+     */
+    // public function envelope(): Envelope
+    // {
+    //     return new Envelope(
+    //         subject: 'Activation Account',
+    //     );
+    // }
+
+    // /**
+    //  * Get the message content definition.
+    //  */
+    // public function content(): Content
+    // {
+    //     return new Content(
+    //         view: 'email.sendemail',
+    //     );
+    // }
+
+    // /**
+    //  * Get the attachments for the message.
+    //  *
+    //  * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+    //  */
+    // public function attachments(): array
+    // {
+    //     return [];
+    // }
+
+    // 'Send Email With Laravel Queue'
+
+    public function build()
+    {
+        return $this->from('noreply@b.com', 'Laravel Mail')
+            ->subject($this->data['subjek'])
+            ->view('result');
+    }
+}
